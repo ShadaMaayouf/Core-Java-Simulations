eb0f40ff4d7353699d8db21ba11b1df24d5ed2fc
ShadaMaayouf <maayouf27@gmail.com>
2024-01-18 01:41:11 +0000

    Add Rowing simulation 1 and 2

--- src/Ruderwettkampf/Rennen.java
+++ src/Ruderwettkampf/Rennen.java
@@ -1,9 +1,74 @@
 package Ruderwettkampf;
-
 import java.util.Random;
 
+public class Rennen {
+    private Boot[] boote;
+    private int distanz;
+    private Random rand;
+
+
+    public Rennen(int anzahlBoote, String bootTyp, int distanz) {
+        this.distanz = distanz;
+        this.rand = new Random();
+        boote = new Boot[anzahlBoote];
+        for (int i = 0; i < anzahlBoote; i++) {
+            switch (bootTyp) {
+                case "Doppelzweier":
+                    boote[i] = new Doppelzweier(this.rand);
+                    break;
+                case "Vierer":
+                    boote[i] = new Vierer(this.rand);
+                    break;
+                default:
+                    System.out.println("Boot muss entweder ein Doppelzweier oder ein Vierer sein.");
+                    return;
+            }
+        }
+    }
+
+    public void start_race() throws InterruptedException {
+
+        int[] positionen = new int[boote.length];
+
+        while (true) {
+            System.out.println(generateTrackLine() + "| Goal");
+            for (int i = 0; i < boote.length; i++) {
+                //int spaces = Math.min(distanz - positionen[i], distanz);
+                for (int j = 0; j < distanz - positionen[i]; j++) {
+                    System.out.print(" ");
+                }
+                System.out.println(boote[i].getAussehen());
+            }
+            //System.out.println("------------------------------| Goal");
+            System.out.println(generateTrackLine() + "| Goal");
+            Thread.sleep(2000);
+
+            for (int i = 0; i < boote.length; i++) {
+                positionen[i] += boote[i].gesamtleistung();
+                if (positionen[i] >= distanz) {
+                    System.out.println("Boot " + (i + 1) + " hat das Ziel erreicht!");
+                    return;
+                }
+            }
+        }
+    }
+
+    private String generateTrackLine() {
+        char[] trackLine = new char[distanz];
+        for (int i = 0; i < distanz; i++) {
+            trackLine[i] = '-';
+        }
+        return new String(trackLine);
+    }
+
+}
+
 class Ruderer {
-    private Random rand = new Random();
+    //private Random rand = new Random();
+    private Random rand;
+    public Ruderer(Random rand) {
+        this.rand = rand;
+    }
 
     public int leistung() {
         return 400 + rand.nextInt(101);
@@ -20,74 +85,32 @@ class Boot {
         }
         return summe;
     }
+    public String getAussehen() {
+        return "<--o-o-o-o-->";
+    }
 }
 
 class Doppelzweier extends Boot {
-    public Doppelzweier() {
+    public Doppelzweier(Random rand) {
         this.ruderer = new Ruderer[2];
         for (int i = 0; i < 2; i++) {
-            this.ruderer[i] = new Ruderer();
+            this.ruderer[i] = new Ruderer(rand);
         }
+    }
+
+    @Override
+    public String getAussehen() {
+        return "<--o-o-->";
     }
 }
 
 class Vierer extends Boot {
-    public Vierer() {
+    public Vierer(Random rand) {
         this.ruderer = new Ruderer[4];
         for (int i = 0; i < 4; i++) {
-            this.ruderer[i] = new Ruderer();
+            this.ruderer[i] = new Ruderer(rand);
         }
     }
 }
 
 
-public class Rennen {
-    private Boot[] boote;
-    private int distanz;
-
-    public Rennen(int anzahlBoote, String bootTyp, int distanz) {
-        this.distanz = distanz;
-        boote = new Boot[anzahlBoote];
-        for (int i = 0; i < anzahlBoote; i++) {
-            switch (bootTyp) {
-                case "Doppelzweier":
-                    boote[i] = new Doppelzweier();
-                    break;
-                case "Vierer":
-                    boote[i] = new Vierer();
-                    break;
-                default:
-                    System.out.println("Boot muss entweder ein Doppelzweier oder ein Vierer sein.");
-                    break;
-            }
-        }
-    }
-
-
-    public void start() throws InterruptedException {
-        int[] positionen = new int[boote.length];
-        while (true) {
-            printRace(positionen);
-            Thread.sleep(1000);
-
-            for (int i = 0; i < boote.length; i++) {
-                positionen[i] += boote[i].gesamtleistung();
-                if (positionen[i] >= distanz) {
-                    System.out.println("Boot " + (i + 1) + " hat das Ziel erreicht!");
-                    return;
-                }
-            }
-        }
-    }
-
-    private void printRace(int[] positionen) {
-        System.out.println("------------------------------| Goal");
-        for (int i = 0; i < boote.length; i++) {
-            for (int j = 0; j < distanz - positionen[i]; j++) {
-                System.out.print(" ");
-            }
-            System.out.println("<--o-o-o-o-->");
-        }
-        System.out.println("------------------------------| Goal");
-    }
-}
--- src/Ruderwettkampf/TestProgramm.java
+++ src/Ruderwettkampf/TestProgramm.java
@@ -1,26 +1,27 @@
 package Ruderwettkampf;
 
+
 public class TestProgramm {
     public static void main(String[] args) throws InterruptedException {
         
-        test_rennen_3_doppelzweier_distanz1000();
-        //test_rennen_4_Vierer_distanz15000();
+        test_rennen_3_doppelzweier_distanz2000();
+        test_rennen_4_Vierer_distanz4000();
 
     }
 
-    private static void test_rennen_3_doppelzweier_distanz1000() {
-        Rennen rennen1 = new Rennen(3, "Doppelzweier", 10000);
+    private static void test_rennen_3_doppelzweier_distanz2000() {
+        Rennen rennen = new Rennen(3, "Doppelzweier", 2000);
         try {
-            rennen1.start();
+            rennen.start_race();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
     }
 
-    private static void test_rennen_4_Vierer_distanz15000() {
-        Rennen rennen2 = new Rennen(4, "Vierer", 15000);
+    private static void test_rennen_4_Vierer_distanz4000() {
+        Rennen rennen = new Rennen(4, "Vierer", 4000);
         try {
-            rennen2.start();
+            rennen.start_race();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
